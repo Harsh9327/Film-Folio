09dda61c71b8dc79a3b66fa3b9dbb49222d964b2
Harsh <harshmungra255@gmail.com>
2024-01-01 19:11:12 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Allroutes } from './routes/Allroutes';
 import { Header, Footer } from "./components";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 // import {ScrollToTop} from "./components/ScrollToTop";
 
 import './App.css';
@@ -8,8 +9,8 @@ function App() {
   return (
     <div className="App">
       <div className='dark:bg-slate-800'>
+        <SpeedInsights/>
       <Header />
-      {/* <ScrollToTop/> */}
       <Allroutes />
       <Footer />
       </div>
